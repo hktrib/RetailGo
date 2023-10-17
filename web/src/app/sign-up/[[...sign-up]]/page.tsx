f6e63aeb1a6886390af97404a051acd4eade1eb6
daxidngyn <daxidnguyen@gmail.com>
2023-10-17 16:21:30 -0700

    init signin and signup pages

---
+++ web/src/app/sign-up/[[...sign-up]]/page.tsx
@@ -0,0 +1,9 @@
+export default function Signup() {
+  return (
+    <main>
+      <div className="mx-auto max-w-2xl py-24 sm:py-40 lg:py-48">
+        <h1>Sign up</h1>
+      </div>
+    </main>
+  );
+}
