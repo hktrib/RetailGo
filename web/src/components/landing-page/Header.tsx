ff3f708df149835c3472fcf2b852a99a89d20ee1
daxidngyn <daxidnguyen@gmail.com>
2023-10-17 16:08:35 -0700

    initialize web app + landing page

---
+++ web/src/components/landing-page/Header.tsx
@@ -0,0 +1,35 @@
+import Link from "next/link";
+
+export default function Header() {
+  return (
+    <header className="absolute inset-x-0 top-0 z-50">
+      <nav
+        className="flex items-center justify-between p-6 lg:px-8"
+        aria-label="Global"
+      >
+        <div className="flex lg:flex-1">
+          <Link href="/" className="-m-1.5 p-1.5">
+            <span className="sr-only">RetailGo</span>
+
+            <span className="font-bold tracking-wide">RetailGo</span>
+          </Link>
+        </div>
+
+        <div className="hidden lg:flex lg:flex-1 lg:justify-end items-center space-x-4">
+          <Link
+            href="/sign-in"
+            className="text-sm font-semibold leading-6 text-gray-900"
+          >
+            Sign in
+          </Link>
+          <Link
+            href="/sign-up"
+            className="bg-amber-500 text-white font-semibold p-2 rounded-md text-sm"
+          >
+            Get started <span aria-hidden="true">&rarr;</span>
+          </Link>
+        </div>
+      </nav>
+    </header>
+  );
+}
