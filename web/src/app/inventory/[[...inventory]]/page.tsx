7ce1bb7b06c3c928ba4666cdd3fe1c219710ca85
Colby <cfrey@protonmail.com>
2023-10-24 21:47:36 -0700

    Update page.tsx

--- web/src/app/inventory/[[...inventory]]/page.tsx
+++ web/src/app/inventory/[[...inventory]]/page.tsx
@@ -11,28 +11,134 @@ export default function Inventory() {
 
   return (
     <main>
-      <div className="mx-auto max-w-2xl py-24 sm:py-40 lg:py-48">
-        <h1>Inventory</h1>
-        <table className="table-auto w-full mt-8">
-          <thead>
-            <tr>
-              <th className="px-4 py-2">Name</th>
-              <th className="px-4 py-2">Description</th>
-              <th className="px-4 py-2">Price</th>
-              <th className="px-4 py-2">Quantity</th>
-            </tr>
-          </thead>
-          <tbody>
-            {inventory.map((item, index) => (
-              <tr key={index}>
-                <td className="border px-4 py-2">{item.name}</td>
-                <td className="border px-4 py-2">{item.description}</td>
-                <td className="border px-4 py-2">${item.price}</td>
-                <td className="border px-4 py-2">{item.quantity}</td>
-              </tr>
-            ))}
-          </tbody>
-        </table>
+      <div className='py-24 sm:py-40 lg:py-48'>
+        <h1 className="text-2xl font-bold mx-auto max-w-7xl">Inventory Stats</h1>
+        <div className=" flex mx-auto max-w-7xl">
+          <Infocard bgColor={"red"} title={"Test"} count={"24"} icon={undefined}></Infocard>
+          <div className="bg-orange-100 rounded-lg p-6 mr-4">
+            <h2 className="text-lg font-bold mb-2">Total Items</h2>
+            <p className="text-3xl font-bold">{inventory.length}</p>
+          </div>
+          <div className="bg-green-100 rounded-lg p-6 mr-4">
+            <h2 className="text-lg font-bold mb-2">TO BE ADDED</h2>
+            <p className="text-3xl font-bold">0</p>
+          </div>
+          <div className="bg-blue-100 rounded-lg p-6 mr-4">
+            <h2 className="text-lg font-bold mb-2">TO BE ADDED</h2>
+            <p className="text-3xl font-bold">0</p>
+          </div>
+          <div className="bg-red-100 rounded-lg p-6">
+            <h2 className="text-lg font-bold mb-2">TO BE ADDED</h2>
+            <p className="text-3xl font-bold">0</p>
+          </div>
+        </div>
+
+        <div className="mx-auto max-w-7xl ">
+          <button className="bg-blue-500 hover-bg-blue-700 text-white font-bold py-2 px-4 rounded mb-4" onClick={handleAddItem}>
+            Add Item
+          </button>
+          {showAddItemForm && (
+            <div className="p-4 border border-gray-300 mb-4">
+              <h2>Add New Item</h2>
+              <div className="mb-4">
+                <label>Name:</label>
+                <input
+                  type="text"
+                  value={newItem.name}
+                  onChange={(e) => setNewItem({ ...newItem, name: e.target.value })}
+                  className="border border-gray-300 rounded-md px-2 py-1"
+                />
+              </div>
+              <div className="mb-4">
+                <label>Description:</label>
+                <input
+                  type="text"
+                  value={newItem.description}
+                  onChange={(e) => setNewItem({ ...newItem, description: e.target.value })}
+                  className="border border-gray-300 rounded-md px-2 py-1"
+
+                />
+              </div>
+              <div className="mb-4">
+                <label>Price:</label>
+                <input
+                  type="number"
+                  value={newItem.price}
+                  onChange={(e) => setNewItem({ ...newItem, price: Number(e.target.value) })}
+                  className="border border-gray-300 rounded-md px-2 py-1"
+
+                />
+              </div>
+              <div className="mb-4">
+                <label>Quantity:</label>
+                <input
+                  type="number"
+                  value={newItem.quantity}
+                  onChange={(e) => setNewItem({ ...newItem, quantity: Number(e.target.value) })}
+                  className="border border-gray-300 rounded-md px-2 py-1"
+
+                />
+              </div>
+              <button
+                className="bg-green-500 hover:bg-green-700 text-white font-bold py-2 px-4 rounded mr-2"
+                onClick={handleSaveItem}
+              >
+                Save
+              </button>
+              <button
+                className="bg-red-500 hover:bg-red-700 text-white font-bold py-2 px-4 rounded"
+                onClick={handleCancelAddItem}
+              >
+                Cancel
+              </button>
+            </div>
+          )}
+          <div className='product-list'>
+            <div className="--flex-between --flex-dir-column">
+              <span>
+                <h3>Inventory Items</h3>
+              </span>
+            </div>
+            <div className="ml-auto">
+              <span>
+                <Search
+                  value={search}
+                  onChange={(e) => setSearch(e.target.value)}
+                />
+              </span>
+            </div>
+            <table className="table">
+              <thead>
+                <tr>
+                  <th className="px-4 py-2">Name</th>
+                  <th className="px-4 py-2">Description</th>
+                  <th className="px-4 py-2">Price</th>
+                  <th className="px-4 py-2">Quantity</th>
+                  <th className="px-4 py-2">Actions</th>
+                </tr>
+              </thead>
+              <tbody>
+                {inventory.map((item, index) => (
+                  <tr key={index}>
+                    <td className="border px-4 py-2">{item.name}</td>
+                    <td className="border px-4 py-2">{item.description}</td>
+                    <td className="border px-4 py-2">${item.price}</td>
+                    <td className="border px-4 py-2">{item.quantity}</td>
+                    <td className="border px-4 py-2">
+                      <button
+                        className="text-red-500 hover:text-red-700"
+                        onClick={() => handleDeleteItem(index)}
+                      >
+                        <Trash size={16} />
+                      </button>
+                    </td>
+                  </tr>
+                ))}
+              </tbody>
+            </table>
+          </div>
+
+        </div>
       </div>
     </main>
   );
