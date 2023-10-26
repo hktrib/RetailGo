ae64482d80629debef88c6845bd6127445b501d5
Colby <cfrey@protonmail.com>
2023-10-26 16:04:05 -0700

    Get_inventory api calls

--- web/src/components/app-page/inventory-table/index.tsx
+++ web/src/components/app-page/inventory-table/index.tsx
@@ -40,6 +40,24 @@ const initialInventory = [
   },
 ];
 
+const callAPI = async () => {
+  try {
+      const res = await fetch(
+          `https://famous-quotes4.p.rapidapi.com/random?category=all&count=2`,
+          {
+              method: 'GET',
+              /*headers: {
+                  'X-RapidAPI-Key': 'your-rapidapi-key',
+                  'X-RapidAPI-Host': 'famous-quotes4.p.rapidapi.com',
+              },*/
+          }
+      );
+      const data = await res.json();
+      console.log(data);
+  } catch (err) {
+      console.log(err);
+  }
+
 export default function InventoryTable() {
   return (
     <div>
