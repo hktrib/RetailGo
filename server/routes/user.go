8099947984e86dd76f86d5a7af0f0c1e0f9d2cb6
hktrib(Apramay Singh) <hktribunal@gmail.com>
2023-10-31 15:28:14 -0700

    SOC for routes

---
+++ server/routes/user.go
@@ -0,0 +1,62 @@
+package server
+
+import (
+	"encoding/json"
+	"net/http"
+	"strconv"
+
+	"github.com/go-chi/chi/v5"
+	"github.com/hktrib/RetailGo/ent"
+	"github.com/hktrib/RetailGo/ent/user"
+)
+
+func (srv *Server) UserDelete(w http.ResponseWriter, r *http.Request) {
+
+	user_id, err := strconv.Atoi(r.URL.Query().Get("user"))
+	if err != nil {
+		http.Error(w, http.StatusText(http.StatusBadRequest), http.StatusBadRequest)
+		return
+	}
+
+	err = srv.Client.
+		User.DeleteOneID(user_id).
+		Exec(r.Context())
+
+	if ent.IsNotFound(err) {
+		http.Error(w, http.StatusText(http.StatusNotFound), http.StatusNotFound)
+		return
+	} else if err != nil {
+		http.Error(w, http.StatusText(http.StatusInternalServerError), http.StatusInternalServerError)
+		return
+	}
+	w.WriteHeader(http.StatusOK)
+	w.Write([]byte("OK"))
+}
+
+func (srv *Server) UserQuery(w http.ResponseWriter, r *http.Request) {
+	ctx := r.Context()
+	user_id, err := strconv.Atoi(chi.URLParam(r, "user_id"))
+	if err != nil {
+		http.Error(w, http.StatusText(http.StatusBadRequest), http.StatusBadRequest)
+		return
+	}
+
+	user_data, err := srv.Client.User.Query().Where(user.ID(user_id)).Only(ctx)
+	if ent.IsNotFound(err) {
+		http.Error(w, http.StatusText(http.StatusNotFound), http.StatusNotFound)
+		return
+	} else if err != nil {
+		http.Error(w, http.StatusText(http.StatusInternalServerError), http.StatusInternalServerError)
+		return
+	}
+
+	res, err := json.MarshalIndent(user_data, "", "")
+
+	if err != nil {
+		http.Error(w, http.StatusText(http.StatusInternalServerError), http.StatusInternalServerError)
+		return
+	}
+
+	w.WriteHeader(http.StatusOK)
+	w.Write(res)
+}
