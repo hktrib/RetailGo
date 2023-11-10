f38fe0eae6dc8dda9b322b27f733a6b55b5ff75c
Giridhar Vadhul <gvadhul@ucsc.edu>
2023-11-10 11:26:55 -0800

    Removed Lodash Dependency

--- web/src/lib/utils.ts
+++ web/src/lib/utils.ts
@@ -1,8 +1,6 @@
 import { type ClassValue, clsx } from "clsx"
 import { twMerge } from "tailwind-merge"
 import { useAuth } from '@clerk/nextjs';
-import {merge} from 'lodash';
-
  
 export function cn(...inputs: ClassValue[]) {
   return twMerge(clsx(inputs))
@@ -16,8 +14,8 @@ export function useFetch() {
   const authenticatedFetch = async (url: string | Request, init?: RequestInit, headers?: any) => {
     return fetch(url, {
       ...init,
-      headers: headers !== undefined? merge(headers, { Authorization: `Bearer ${await getToken()}` }) : { Authorization: `Bearer ${await getToken()}` }
-    }).then(res => res.json());
+      headers: headers !== undefined? {...headers, ...{ Authorization: `Bearer ${await getToken()}` }} : {...headers, ...{ Authorization: `Bearer ${await getToken()}` }}}
+    ).then(res => res.json());
   };
  
   return authenticatedFetch;
