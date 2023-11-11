a3e5f6838f03559d590bf19982e5de10d00bd84e
Colby <cfrey@protonmail.com>
2023-11-10 18:36:17 -0800

    employee page creation

---
+++ web/src/app/(app-page)/app/employees/page.tsx
@@ -0,0 +1,15 @@
+import React from 'react';
+
+export default function Employees() {
+    return (
+        <div>
+            <h1>Employees Page</h1>
+            <p>Welcome to the Employees Page!</p>
+            <ul>
+                <li>Employee 1</li>
+                <li>Employee 2</li>
+                <li>Employee 3</li>
+            </ul>
+        </div>
+    );
+}
