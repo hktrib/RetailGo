9e12120fc4a7f23477643401e05e6d01d0c2262f
David <daxidnguyen@gmail.com>
2023-12-05 14:53:24 -0800

    [web] fixed navigation

--- web/src/components/app-page/mobile-nav.tsx
+++ web/src/components/app-page/mobile-nav.tsx
@@ -1,44 +1,26 @@
-import { UserButton, useUser } from "@clerk/nextjs";
-import {
-  HelpCircle,
-  HomeIcon,
-  Menu,
-  Package2,
-  Settings,
-  ShoppingBag,
-  Users2,
-} from "lucide-react";
+"use client";
 
+import { useParams } from "next/navigation";
+import { UserButton } from "@clerk/nextjs";
+import { Navigation } from "./sidebar";
 import {
   Sheet,
-  SheetClose,
   SheetContent,
-  SheetFooter,
   SheetHeader,
   SheetTitle,
   SheetTrigger,
 } from "@/components/ui/sheet";
 import { Button } from "@/components/ui/button";
-import Link from "next/link";
-import { useSelectedStore } from "../storeprovider";
-import { useEffect, useState } from "react";
-import { IStore } from "@/models/store";
+import { Menu } from "lucide-react";
 
-const navigation = [
-  { name: "Dashboard", href: "/", icon: HomeIcon },
-  { name: "Employees", href: "/employees", icon: Users2 },
-  { name: "Inventory", href: "/inventory", icon: Package2 },
-  { name: "POS", href: "/pos", icon: ShoppingBag },
-];
+import type { StoreMetadata } from "@/app/(app-page)/app/layout";
 
-const fakeStores = [{ name: "RetailGo", id: "123" }];
-
-export default function MobileNav() {
+export default function MobileNav({ stores }: { stores: StoreMetadata[] }) {
   return (
     <div className="xl:hidden block bg-white/75 border-b">
       <div className="px-6 md:px-8 h-12 flex items-center justify-between">
         <div className="flex items-center gap-x-2">
-          <SidebarNav />
+          <SidebarNav stores={stores} />
 
           <span className="font-bold">RetailGo</span>
         </div>
@@ -50,31 +32,14 @@ export default function MobileNav() {
   );
 }
 
-const SidebarNav = () => {
-  const { user } = useUser();
-  const { selectedStore, selectStore } = useSelectedStore();
-  const [stores, setStores] = useState<IStore[]>([]);
+const SidebarNav = ({ stores }: { stores: StoreMetadata[] }) => {
+  const { store_id } = useParams();
 
-  const mapMetadataStores = (storesData: any[]) =>
-    storesData.map((store) => ({
-      id: store.id,
-      storename: store.storename,
-      Permission_level: store.Permission_level,
-    }));
-
-  useEffect(() => {
-    async function fetchStores() {
-      if (user && user.publicMetadata.stores) {
-        const metadataStores = mapMetadataStores(
-          user.publicMetadata.stores as any[]
-        );
-        setStores(metadataStores);
-      }
-    }
-    if (user && stores) {
-      fetchStores();
-    }
-  }, [user, selectStore]);
+  let currentStore;
+  if (store_id) {
+    const store = stores.filter((store) => store.id === Number(store_id));
+    currentStore = store[0];
+  }
 
   return (
     <Sheet>
@@ -84,75 +49,22 @@ const SidebarNav = () => {
         </Button>
       </SheetTrigger>
       <SheetContent side="left" className="h-full w-[300px]">
-        <SheetHeader className="border-b pb-5">
-          <div className="px-3 py-1.5 flex items-center gap-x-3 bg-gray-100 rounded-md shadow">
-            <div className="h-4 w-4 bg-white flex items-center justify-center rounded-md">
-              <span className="text-xs">
-                {selectedStore?.storename.charAt(0)}
-              </span>
+        <SheetHeader className={`${store_id && "border-b pb-5"}`}>
+          {currentStore && (
+            <div className="px-3 py-1.5 flex items-center gap-x-3 bg-gray-100 rounded-md shadow">
+              <div className="h-4 w-4 bg-white flex items-center justify-center rounded-md">
+                <span className="text-xs">
+                  {currentStore.storename.charAt(0)}
+                </span>
+              </div>
+              <SheetTitle className="text-sm">
+                {currentStore.storename}
+              </SheetTitle>
             </div>
-            <SheetTitle className="text-sm">
-              {" "}
-              {selectedStore?.storename}{" "}
-            </SheetTitle>
-          </div>
+          )}
         </SheetHeader>
 
-        <nav className="flex flex-1 flex-col h-full">
-          <ul role="list" className="flex flex-1 flex-col">
-            <li className="py-5 border-b">
-              <div className="text-xs text-gray-600 mb-2.5 px-3">
-                Navigation
-              </div>
-              <ul role="list" className="space-y-1.5">
-                {navigation.map((item) => (
-                  <li key={item.name}>
-                    <Link
-                      href={`/app/${item.href}`}
-                      className="text-gray-900 hover:text-black flex items-center gap-x-3 px-3 hover:bg-gray-100 py-1.5 rounded-md"
-                    >
-                      <item.icon className="w-4 h-4" aria-hidden="true" />
-                      <span className="text-sm">{item.name}</span>
-                    </Link>
-                  </li>
-                ))}
-              </ul>
-            </li>
-            <li className="py-5 border-b">
-              <div className="text-xs text-gray-600 mb-2.5 px-3">
-                Your stores
-              </div>
-              <ul role="list" className="space-y-1.5">
-                {stores.map((store) => (
-                  <li key={store.storename}>
-                    <div className="group text-gray-900 hover:text-black flex items-center gap-x-3 px-3 hover:bg-gray-100 py-1.5 rounded-md">
-                      <div className="bg-gray-100 group-hover:bg-white h-4 w-4 flex items-center justify-center rounded-md">
-                        <span className="text-xs">
-                          {store.storename.charAt(0)}
-                        </span>
-                      </div>
-                      <span className="text-sm">{store.storename}</span>
-                    </div>
-                  </li>
-                ))}
-              </ul>
-            </li>
-
-            <li className="mt-auto pb-12">
-              <ul role="list" className="space-y-1.5">
-                <li className="flex items-center px-3 py-1.5 gap-x-3">
-                  <Settings className="w-4 h-4" aria-hidden="true" />
-                  <span className="text-sm text-gray-900">Settings</span>
-                </li>
-
-                <li className="flex items-center px-3 py-1.5 gap-x-3">
-                  <HelpCircle className="w-4 h-4" aria-hidden="true" />
-                  <span className="text-sm text-gray-900">Help</span>
-                </li>
-              </ul>
-            </li>
-          </ul>
-        </nav>
+        <Navigation userStores={stores} currentStoreId={store_id as string} />
       </SheetContent>
     </Sheet>
   );
