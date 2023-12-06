5ba78c08feb1c87e017f0add08894fe9e7f912a2
Giridhar Vadhul <gvadhul@ucsc.edu>
2023-12-06 00:58:20 -0800

    Switched to Word2Vec (we might have a memory issue after all

--- recommender/server/main.py
+++ recommender/server/main.py
@@ -1,18 +1,13 @@
 from fastapi import FastAPI, status, Response, HTTPException
 from data_models import Item, ItemBatch
 from vectorize import Vectorizer
-from weaviate_writer import WeaviateWriter
+from weaviate import Weaviate
 import numpy as np
 
 app = FastAPI()
 
 CLIP_vectorizer = Vectorizer()
-Writer = WeaviateWriter()
-
-@app.get("/recommend/{store_id}", status_code = 200)
-
-def recommend(store_id: int):
-    pass
+WeaviateClient = Weaviate()
 
 @app.post("/vectorizeItems", status_code = 201)
 def vectorize_batch(batch: ItemBatch, response: Response):
@@ -33,10 +28,10 @@ def vectorize_batch(batch: ItemBatch, response: Response):
 
     for item, vector in zip(batch.items, vectors):
         try:
-            Writer.write_item_vector(item, vector)
+            WeaviateClient.write_item_vector(item, vector)
             # If you are going to reach a change in store, write the discounted average to the store vector as well
             if current_store_id and item.StoreId != current_store_id:
-                Writer.write_store_vector(item.StoreId, todays_store_vector / todays_sales if todays_sales > 0 else todays_store_vector)
+                WeaviateClient.write_store_vector(item.StoreId, todays_store_vector / todays_sales if todays_sales > 0 else todays_store_vector)
                 todays_store_vector *= 0
                 todays_sales = 0
             todays_store_vector += item.NumberSoldSinceUpdate * vector    
@@ -47,7 +42,7 @@ def vectorize_batch(batch: ItemBatch, response: Response):
             failures.add(item.ID)
 
     try:
-        Writer.write_store_vector(item.StoreId, todays_store_vector / todays_sales)
+        WeaviateClient.write_store_vector(item.StoreId, todays_store_vector / todays_sales)
         todays_store_vector *= 0
         todays_sales = 0
         
@@ -57,3 +52,11 @@ def vectorize_batch(batch: ItemBatch, response: Response):
     # Send back a JSON with all the item ids that were not successfully written to Weaviate. (If none, assume all went well).   
         
     return {"idsFailedToVectorize": list(failures)}
+
+@app.get("/recommend/{store_id}", status_code = 200)
+
+def recommend(store_id: int):
+    # Retrieve the store vector
+    store_vector = WeaviateClient.get_store_vector(store_id)
+    # Search by the store vector for items
+    return WeaviateClient.search(store_vector)
