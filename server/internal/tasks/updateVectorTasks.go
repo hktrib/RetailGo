7cac0c1091747334ea0135d55eb65754a66bb731
Giridhar Vadhul <gvadhul@ucsc.edu>
2023-12-05 23:42:11 -0800

    Updated Rec Server URL

--- server/internal/tasks/updateVectorTasks.go
+++ server/internal/tasks/updateVectorTasks.go
@@ -23,7 +23,7 @@ const (
 
 // Need to marshall items to JSON, send them to the Python Server, receive the response, read the body, read the ids that were not vectorized, and set them
 func vectorize(unvectorizedItems []*ent.Item) ([]int, error) {
-	REC_SERVER_URL := "https://retailgo-production.up.railway.app/"
+	REC_SERVER_URL := "https://recommendation-server-production.up.railway.app"
 	// Marshall items to JSON
 	requestBody, err := json.Marshal(unvectorizedItems)
 	var ids []int
