9d0339dce8ac30b17923ec816cf10bc9c27c62e4
Jonathan Michel <jomichel@ucsc.edu>
2023-12-05 19:15:43 -0800

    live for real

--- server/cmd/api/misc.go
+++ server/cmd/api/misc.go
@@ -1,11 +1,8 @@
 package server
 
 import (
-	"context"
 	"encoding/json"
 	"github.com/hktrib/RetailGo/internal/ent"
-	"github.com/hktrib/RetailGo/internal/ent/category"
-	"github.com/hktrib/RetailGo/internal/ent/item"
 )
 
 // ClientItem item that is sent to the client
@@ -16,26 +13,28 @@ func (ite ClientItem) MarshalJSON() ([]byte, error) {
 }
 
 func MarshalItem(TargetItem ClientItem) ([]byte, error) {
-	var temp ent.Client
-	cats, err := temp.Category.Query().Where(category.HasItemsWith(item.ID(TargetItem.ID))).All(context.Background())
-	var catNames []string
-	if err != nil {
-		if ent.IsNotFound(err) {
-			catNames[0] = "Uncategorized"
-		} else {
-			return nil, err
+	/*
+		cats, err := temp.Category.Query().Where(category.HasItemsWith(item.ID(TargetItem.ID))).All(context.Background())
+		var catNames []string
+		if err != nil {
+			if ent.IsNotFound(err) {
+				catNames[0] = "Uncategorized"
+			} else {
+				return nil, err
+			}
 		}
-	}
-	for _, cat := range cats {
-		catNames = append(catNames, cat.Name)
-	}
+		for _, cat := range cats {
+			catNames = append(catNames, cat.Name)
+		}
+
+	*/
 	return json.Marshal(map[string]interface{}{
 		"id":       TargetItem.ID,
 		"name":     TargetItem.Name,
 		"photo":    TargetItem.Photo,
 		"quantity": TargetItem.Quantity,
 		"price":    TargetItem.Price,
-		"category": catNames[0],
+		"category": TargetItem.CategoryName,
 	})
 }
 func PruneItems(TargetItems ...*ent.Item) []ClientItem {
