720175c6c44b1e89a3bff2d6f2853a4cf8b252ae
Colby <cfrey@protonmail.com>
2023-12-14 13:43:14 -0800

    Undoing 15f89b5c3d5047f1ec3573341562396d279380a4

--- web/src/app/(app-page)/store/layout.tsx
+++ web/src/app/(app-page)/store/layout.tsx
@@ -1,10 +1,11 @@
-import { auth } from "@clerk/nextjs";
+import { auth, clerkClient } from "@clerk/nextjs";
 import { notFound, redirect } from "next/navigation";
 
 import { Toaster } from "react-hot-toast";
 import MobileNav from "@/components/app-page/mobile-nav";
 import Sidebar from "@/components/app-page/sidebar";
 import Providers from "../providers";
+import { GetStoresByClerkId } from "./queries";
 
 export type StoreMetadata = {
   Permission_level: number;
@@ -12,25 +13,39 @@ export type StoreMetadata = {
   storename: string;
 };
 
-export default function AppLayout({ children }: { children: React.ReactNode }) {
-  const { sessionClaims } = auth();
+export default async function AppLayout({ children }: { children: React.ReactNode }) {
+  const {userId, sessionClaims } = auth();
 
   if (!sessionClaims || !sessionClaims.publicMetadata) return notFound();
 
   const publicMetadata = sessionClaims.publicMetadata as {
     stores?: StoreMetadata[];
   };
-  if (!publicMetadata.stores) { 
-    // Should work
-    console.log("No public metadata -> redirecting to main page");
-    redirect("/");
+
+  if (!publicMetadata.stores) {
+    console.log("No public metadata getting from db");
+    const db_stores = await GetStoresByClerkId();
+    console.log("Stores are: ", db_stores.stores);
+
+    if(db_stores.success){
+      await clerkClient.users.updateUserMetadata(userId, {
+        publicMetadata: {
+          stores: db_stores.stores
+        }
+      })
+      
+    }
+    else{
+      console.log("No public metadata -> redirecting to register-store");
+      redirect("/register-store");
+    }
   }
 
   return (
     <Providers>
       <div className="flex h-full min-h-screen flex-col dark:bg-zinc-800">
-        <MobileNav stores={publicMetadata.stores} />
-        <Sidebar stores={publicMetadata.stores} />
+        <MobileNav stores={publicMetadata.stores ?? []} />
+        <Sidebar stores={publicMetadata.stores ?? []} />
         <div className="flex h-full flex-grow flex-col xl:pl-64">
           <div className="flex h-full flex-grow flex-col p-2">
             <div className="flex h-full flex-grow flex-col rounded-3xl border border-gray-100 shadow-sm dark:border-zinc-800 dark:bg-zinc-900">
